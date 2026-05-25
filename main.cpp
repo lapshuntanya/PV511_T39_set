3ddc14d6524c2fa19dada775e3db5bb55008e45b
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-05-25 19:18:45 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include "BinTree.h"
 
 int main() {
 
+    BinTree<int> tree;
+
+    tree.insert(8);
+    tree.insert(11);
+    tree.insert(2);
+    tree.insert(0);
+    tree.insert(66);
+    tree.insert(1);
+    tree.insert(7);
+    tree.insert(656);
+    tree.insert(13);
+    tree.insert(57);
+
+
+    cout << endl;
     return 0;
 }
