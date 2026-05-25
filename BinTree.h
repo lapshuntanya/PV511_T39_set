a23ded716c6bcbb9aa2191f05ab0382f0b320370
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-05-25 19:10:22 +0300

    insert Node

--- BinTree.h
+++ BinTree.h
@@ -15,3 +15,51 @@ public:
         cout << "\t Left: " << left << " Right: " << right << endl;
     }
 };
+
+
+template <typename U>
+class BinTree {
+    Node<U>* root; //Вказівник на перший вузол у дереві
+public:
+    BinTree() { root = nullptr; }
+    ~BinTree() {}
+
+    void insert(U value) {
+       Node<U>* item = new Node<U>(value);
+
+        if (root == nullptr)
+            root = item;
+        else {
+            //Знаходимо місце для вставки вузла
+            Node<U>* p = root;
+            Node<U>* prevPlace = root;
+            while (p != nullptr) {
+               // if (item->info < p->info) p = p->left;
+               // else p = p->right;
+                prevPlace = p;
+                p = (item->info < p->info) ? p->left : p->right;
+            }
+
+            //Привʼязуємно новий вузол до дерева
+            if (item->info < prevPlace->info)
+                prevPlace->left = item;
+            else
+                prevPlace->right = item;
+        }
+    }
+
+    Node<U>* search(U value) {
+        //Повернути адресу вузла зі значенням value
+        //Якщо value немає в дереві, то повернути nullptr
+    }
+
+    //Повний обхід дерева
+    void showTree() {
+        if (root == nullptr)
+            cout << "Дерево контейнерів порожнє!" << endl;
+        else {
+            // . . .
+        }
+    }
+
+};
